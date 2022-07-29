9820ec8f275c7c891342b64831e4c8bbd68a88a4
Terry Tan <59902670+t3reetan@users.noreply.github.com>
2022-07-29 01:37:03 -0700

    Revert "Update helper-hardhat-config.ts"

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,10 +1,6 @@
 export const networkConfig = {
-  localhost: { // the one that sets up 20 accounts
-    chainId: 31337,
-  }, 
-  hardhat: {
-    chainId: 31337,
-  },
+  localhost: {}, // the one that sets up 20 accounts
+  hardhat: {},
   rinkeby: {
     chainId: 4,
     ethPriceFeedAddress: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
